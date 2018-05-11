b2cb3ad037b7c54393e8af8bfc025d4f087acb5d
hedongyang <2732067273@qq.com>
2018-05-11 12:29:49 +0800

    1.新增react-nvtive版的jhipster demo

---
+++ xDemoHybrid/App/Config/AppConfig.js
@@ -0,0 +1,8 @@
+// Simple React Native specific changes
+
+export default {
+  apiUrl: 'http://localhost:8080/',
+  appUrlScheme: 'xdemohybrid',
+  // font scaling override - RN default is on
+  allowTextFontScaling: true
+}
