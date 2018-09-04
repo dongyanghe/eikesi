238a45f9a576fd850e2a07e9719b017f5350344e
hedongyang <2732067273@qq.com>
2018-09-04 23:52:38 +0800

    “im-web:

--- x-im-web/src/main/webapp/app/shared/UserList/index.tsx
+++ x-im-web/src/main/webapp/app/shared/UserList/index.tsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-
-import './style.csss';
+import './style.scss';
 
 export interface IProps {
   max: PropTypes.number.isRequired;
