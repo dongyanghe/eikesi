4dd8ed622f532d1a183c0ae08183e597a169b071
hedongyang <2732067273@qq.com>
2018-09-04 22:04:00 +0800

    im-web: 1.修复部分样式错误 2.修复样式文件引入错误

--- x-im-web/src/main/webapp/app/shared/UserList/index.tsx
+++ x-im-web/src/main/webapp/app/shared/UserList/index.tsx
@@ -1,199 +1,186 @@
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import './style.css';
+import './style.csss';
 
 export interface IProps {
-    max: PropTypes.number.isRequired;
-    searching: PropTypes.string.isRequired;
-    search: PropTypes.func.isRequired;
-    getList: PropTypes.func.isRequired;
-    onChange: PropTypes.func.isRequired;
-  }
+  max: PropTypes.number.isRequired;
+  searching: PropTypes.string.isRequired;
+  search: PropTypes.func.isRequired;
+  getList: PropTypes.func.isRequired;
+  onChange: PropTypes.func.isRequired;
+}
 export interface IState {
-    selected: any[];
-    active: string;
-  }
+  selected: any[];
+  active: string;
+}
 export default class UserList extends Component<IProps, IState> {
-    timer: any;
-    listRef: any;
-    inputRef: any;
-    props = {
-        max: 20,
-        ...this.props
-    };
-    state = {
-        selected: [],
-        active: ''
-    };
+  timer: any;
+  listRef: any;
+  inputRef: any;
+  props = {
+    max: 20,
+    ...this.props
+  };
+  state = {
+    selected: [],
+    active: ''
+  };
 
-    highlight(offset) {
-        let scroller = this.listRef;
-        let users = Array.from(this.listRef.querySelectorAll('li[data-userid]'));
-        let index = users.findIndex(e => e.classList.contains('active'));
+  highlight(offset) {
+    let scroller = this.listRef;
+    let users = Array.from(this.listRef.querySelectorAll('li[data-userid]'));
+    let index = users.findIndex(e => e.classList.contains('active'));
 
-        if (index > -1) {
-            users[index].classList.remove('active');
-        }
-
-        index += offset;
-
-        if (index < 0) {
-            // Fallback to the last element
-            index = users.length - 1;
-        } else if (index > users.length - 1) {
-            // Fallback to the 1th element
-            index = 0;
-        }
-
-        let active = users[index];
-
-        if (active) {
-            // Keep active item always in the viewport
-            active.classList.add('active');
-            scroller.scrollTop = active.offsetTop + active.offsetHeight - scroller.offsetHeight;
-        }
+    if (index > -1) {
+      users[index].classList.remove('active');
     }
 
-    navigation(e) {
-        const keyCode = e.keyCode;
-        const offset = {
-            // Up
-            '38': -1,
-            '40': 1
-        }[keyCode];
+    index += offset;
 
-        if (offset) {
-            this.highlight(offset);
-        }
-
-        if (keyCode !== 13) {
-            return;
-        }
-
-        const active = this.listRef.querySelector(`.active`);
-
-        if (active) {
-            const userid = active.dataset.userid;
-
-            if (!this.state.selected.includes(userid)) {
-                // Add
-                this.addSelected(userid, userid);
-            } else {
-                // Remove
-                this.removeSelected(userid, userid);
-            }
-            setTimeout(() => this.props.onChange(this.state.selected));
-        }
+    if (index < 0) {
+      // Fallback to the last element
+      index = users.length - 1;
+    } else if (index > users.length - 1) {
+      // Fallback to the 1th element
+      index = 0;
     }
 
-    search(text) {
-        clearTimeout(this.timer);
+    let active = users[index];
 
-        this.timer = setTimeout(() => {
-            this.props.search(text);
-        }, 300);
+    if (active) {
+      // Keep active item always in the viewport
+      active.classList.add('active');
+      scroller.scrollTop = active.offsetTop + active.offsetHeight - scroller.offsetHeight;
+    }
+  }
+
+  navigation(e) {
+    const keyCode = e.keyCode;
+    const offset = {
+      // Up
+      '38': -1,
+      '40': 1
+    }[keyCode];
+
+    if (offset) {
+      this.highlight(offset);
     }
 
-    addSelected(userid, active = this.state.active) {
-        let selected = [
-            userid,
-            ...this.state.selected
-        ];
-        const max = this.props.max;
-
-        if (max > 0) {
-            selected = selected.slice(0, this.props.max);
-        }
-
-        this.setState({
-            active,
-            selected
-        });
-        setTimeout(() => this.props.onChange(this.state.selected));
+    if (keyCode !== 13) {
+      return;
     }
 
-    removeSelected(userid, active = this.state.active) {
-        const selected = this.state.selected;
-        const index = selected.indexOf(userid);
+    const active = this.listRef.querySelector(`.active`);
 
-        this.setState({
-            active,
-            selected: [
-                ...selected.slice(0, index),
-                ...selected.slice(index + 1, selected.length)
-            ]
-        });
-        setTimeout(() => this.props.onChange(this.state.selected));
+    if (active) {
+      const userid = active.dataset.userid;
+
+      if (!this.state.selected.includes(userid)) {
+        // Add
+        this.addSelected(userid, userid);
+      } else {
+        // Remove
+        this.removeSelected(userid, userid);
+      }
+      setTimeout(() => this.props.onChange(this.state.selected));
+    }
+  }
+
+  search(text) {
+    clearTimeout(this.timer);
+
+    this.timer = setTimeout(() => {
+      this.props.search(text);
+    }, 300);
+  }
+
+  addSelected(userid, active = this.state.active) {
+    let selected = [userid, ...this.state.selected];
+    const max = this.props.max;
+
+    if (max > 0) {
+      selected = selected.slice(0, this.props.max);
     }
 
-    toggleSelected(userid) {
-        if (!this.state.selected.includes(userid)) {
-            // Add
-            this.addSelected(userid);
-        } else {
-            // Remove
-            this.removeSelected(userid);
-        }
+    this.setState({
+      active,
+      selected
+    });
+    setTimeout(() => this.props.onChange(this.state.selected));
+  }
 
-        setTimeout(() => this.inputRef.focus());
+  removeSelected(userid, active = this.state.active) {
+    const selected = this.state.selected;
+    const index = selected.indexOf(userid);
+
+    this.setState({
+      active,
+      selected: [...selected.slice(0, index), ...selected.slice(index + 1, selected.length)]
+    });
+    setTimeout(() => this.props.onChange(this.state.selected));
+  }
+
+  toggleSelected(userid) {
+    if (!this.state.selected.includes(userid)) {
+      // Add
+      this.addSelected(userid);
+    } else {
+      // Remove
+      this.removeSelected(userid);
     }
 
-    renderList() {
-        const { searching, getList } = this.props;
-        const list = getList();
+    setTimeout(() => this.inputRef.focus());
+  }
 
-        if (searching && list.length === 0) {
-            return (
-                <li className={'notfound'}>
-                    <img src="assets/images/crash.png" />
-                    <h3>找不到好友 '{searching}'</h3>
-                </li>
-            );
-        }
+  renderList() {
+    const { searching, getList } = this.props;
+    const list = getList();
 
-        return list.map((e, index) =>
-             (
-                <li
-                    className={classnames({
-                        'selected': this.state.selected.includes(e.UserName),
-                        'active': this.state.active === e.UserName
-                    })}
-                    data-userid={e.UserName}
-                    key={index}
-                    onClick={ev => this.toggleSelected(e.UserName)}>
-                    <img
-                        className={'avatar'}
-                        src={e.HeadImgUrl} />
-                    <span
-                        className={'username'}
-                        dangerouslySetInnerHTML={{ __html: e.RemarkName || e.NickName }} />
-
-                    <i className="icon-ion-android-done-all" />
-                </li>
-            )
-        );
+    if (searching && list.length === 0) {
+      return (
+        <li className={'notfound'}>
+          <img src="assets/images/crash.png" />
+          <h3>找不到好友 '{searching}'</h3>
+        </li>
+      );
     }
 
-    render() {
-        return (
-            <div className={'container'}>
-                <input
-                    ref={this.inputRef}
-                    autoFocus={true}
-                    onKeyUp={e => this.navigation(e)}
-                    onInput={e => this.search(this.inputRef.value)}
-                    placeholder="Type to Search..."
-                    type="text" />
+    return list.map((e, index) => (
+      <li
+        className={classnames({
+          selected: this.state.selected.includes(e.UserName),
+          active: this.state.active === e.UserName
+        })}
+        data-userid={e.UserName}
+        key={index}
+        onClick={ev => this.toggleSelected(e.UserName)}
+      >
+        <img className={'avatar'} src={e.HeadImgUrl} />
+        <span className={'username'} dangerouslySetInnerHTML={{ __html: e.RemarkName || e.NickName }} />
 
-                <ul
-                    className={'list'}
-                    ref={this.listRef}>
-                    {this.renderList()}
-                </ul>
-            </div>
-        );
-    }
+        <i className="icon-ion-android-done-all" />
+      </li>
+    ));
+  }
+
+  render() {
+    return (
+      <div className={'container'}>
+        <input
+          ref={this.inputRef}
+          autoFocus={true}
+          onKeyUp={e => this.navigation(e)}
+          onInput={e => this.search(this.inputRef.value)}
+          placeholder="Type to Search..."
+          type="text"
+        />
+
+        <ul className={'list'} ref={this.listRef}>
+          {this.renderList()}
+        </ul>
+      </div>
+    );
+  }
 }
