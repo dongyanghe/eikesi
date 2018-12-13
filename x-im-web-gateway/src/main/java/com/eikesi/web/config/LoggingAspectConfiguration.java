89d7eb8b1248839cc3f410797f79481d7b348806
hedongyang <2732067273@qq.com>
2018-12-14 00:05:09 +0800

    Initial application generated by JHipster-5.7.0

---
+++ x-im-web-gateway/src/main/java/com/eikesi/web/config/LoggingAspectConfiguration.java
@@ -0,0 +1,19 @@
+package com.eikesi.web.config;
+
+import com.eikesi.web.aop.logging.LoggingAspect;
+
+import io.github.jhipster.config.JHipsterConstants;
+
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ x-im-web-gateway/src/main/java/com/eikesi/web/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.eikesi.web.repository;
+
+import com.eikesi.web.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
