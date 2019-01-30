58a9dd833fca817d968107d46be9f8b135e2210b
hedongyang <2732067273@qq.com>
2019-01-30 21:06:23 +0800

    Initial application generated by JHipster-5.8.1

---
+++ x-im-monolith/src/main/java/com/eikesi/im/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.eikesi.im.repository;
+
+import com.eikesi.im.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
