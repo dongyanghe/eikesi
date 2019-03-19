fa62a0c4f45404ac8aa6ac8c996032e13e0e2f2d
hedongyang <2732067273@qq.com>
2019-03-20 00:09:40 +0800

    Initial application generated by JHipster-5.8.2

---
+++ x-im-message-server/src/test/java/com/eikesi/im/message/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,9 @@
+package com.eikesi.im.message.cucumber.stepdefs;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
