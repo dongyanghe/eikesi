38780ef2da6b5e2324a49a0ede5c5e8b8c1bed4e
hedongyang <2732067273@qq.com>
2019-03-26 01:14:59 +0800

    Initial application generated by JHipster-5.8.2

---
+++ x-im-web-new/src/main/webapp/app/shared/reducers/authentication.ts
@@ -0,0 +1,134 @@
+import axios from 'axios';
+import { Storage } from 'react-jhipster';
+
+import { REQUEST, SUCCESS, FAILURE } from 'app/shared/reducers/action-type.util';
+import { setLocale } from 'app/shared/reducers/locale';
+
+export const ACTION_TYPES = {
+  LOGIN: 'authentication/LOGIN',
+  GET_SESSION: 'authentication/GET_SESSION',
+  LOGOUT: 'authentication/LOGOUT',
+  CLEAR_AUTH: 'authentication/CLEAR_AUTH',
+  ERROR_MESSAGE: 'authentication/ERROR_MESSAGE'
+};
+
+const initialState = {
+  loading: false,
+  isAuthenticated: false,
+  loginSuccess: false,
+  loginError: false, // Errors returned from server side
+  showModalLogin: false,
+  account: {} as any,
+  errorMessage: null as string, // Errors returned from server side
+  redirectMessage: null as string,
+  sessionHasBeenFetched: false,
+  idToken: null as string,
+  logoutUrl: null as string
+};
+
+export type AuthenticationState = Readonly<typeof initialState>;
+
+// Reducer
+
+export default (state: AuthenticationState = initialState, action): AuthenticationState => {
+  switch (action.type) {
+    case REQUEST(ACTION_TYPES.LOGIN):
+    case REQUEST(ACTION_TYPES.GET_SESSION):
+      return {
+        ...state,
+        loading: true
+      };
+    case FAILURE(ACTION_TYPES.LOGIN):
+      return {
+        ...initialState,
+        errorMessage: action.payload,
+        showModalLogin: true,
+        loginError: true
+      };
+    case FAILURE(ACTION_TYPES.GET_SESSION):
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: false,
+        sessionHasBeenFetched: true,
+        showModalLogin: true,
+        errorMessage: action.payload
+      };
+    case SUCCESS(ACTION_TYPES.LOGIN):
+      return {
+        ...state,
+        loading: false,
+        loginError: false,
+        showModalLogin: false,
+        loginSuccess: true
+      };
+    case ACTION_TYPES.LOGOUT:
+      return {
+        ...initialState,
+        showModalLogin: true
+      };
+    case SUCCESS(ACTION_TYPES.GET_SESSION): {
+      const isAuthenticated = action.payload && action.payload.data && action.payload.data.activated;
+      return {
+        ...state,
+        isAuthenticated,
+        loading: false,
+        sessionHasBeenFetched: true,
+        account: action.payload.data
+      };
+    }
+    case ACTION_TYPES.ERROR_MESSAGE:
+      return {
+        ...initialState,
+        showModalLogin: true,
+        redirectMessage: action.message
+      };
+    case ACTION_TYPES.CLEAR_AUTH:
+      return {
+        ...state,
+        loading: false,
+        showModalLogin: true,
+        isAuthenticated: false
+      };
+    default:
+      return state;
+  }
+};
+
+export const displayAuthError = message => ({ type: ACTION_TYPES.ERROR_MESSAGE, message });
+
+export const getSession = () => async (dispatch, getState) => {
+  await dispatch({
+    type: ACTION_TYPES.GET_SESSION,
+    payload: axios.get('uaaserver/api/account')
+  });
+
+  const { account } = getState().authentication;
+  if (account && account.langKey) {
+    const langKey = Storage.session.get('locale', account.langKey);
+    await dispatch(setLocale(langKey));
+  }
+};
+
+export const login = (username, password, rememberMe = false) => async (dispatch, getState) => {
+  const result = await dispatch({
+    type: ACTION_TYPES.LOGIN,
+    payload: axios.post('auth/login', { username, password })
+  });
+  await dispatch(getSession());
+};
+
+export const logout = () => async dispatch => {
+  await dispatch({
+    type: ACTION_TYPES.LOGOUT,
+    payload: axios.post('auth/logout', {})
+  });
+  dispatch(getSession());
+};
+
+export const clearAuthentication = messageKey => (dispatch, getState) => {
+  dispatch(displayAuthError(messageKey));
+  dispatch({
+    type: ACTION_TYPES.CLEAR_AUTH
+  });
+};
